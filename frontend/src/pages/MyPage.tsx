00887ec8b0f535779879d8b6197403a6795c8a92
Gold-gyu <lkg2097@naver.com>
2024-07-23 00:01:51 +0900

    router 설정

---
+++ frontend/src/pages/MyPage.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class MyPage extends Component {
+  render() {
+    return (
+      <div>MyPage</div>
+    )
+  }
+}
---
+++ frontend/src/router/AppRouter.tsx
@@ -0,0 +1,21 @@
+// import App from '@/App';
+import HomePage from '@/pages/HomePage';
+import MyPage from '@/pages/MyPage';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import path from 'path';
+import ErrorPage from '@/pages/ErrorPage';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="*" element={<Navigate replace to="/error" />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
