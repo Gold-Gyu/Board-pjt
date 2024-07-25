165efc3f2fb514963f7fb303e74377a43e5290dc
goldgyu <lkg2097@naver.com>
2024-07-25 21:12:41 +0900

    구조 설계중

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
-import HomePageNavBar from '@/components/HomePageCompo/HomePageNavBar';
+import HomePageContext from '@/components/HomePageCompo/HomePageContext/HomePageContext';
+import HomePageNavBar from '@/components/HomePageCompo/HomePageNavBar/HomePageNavBar';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <HomePageNavBar />
+      <HomePageContext />
     </div>
   );
 };
