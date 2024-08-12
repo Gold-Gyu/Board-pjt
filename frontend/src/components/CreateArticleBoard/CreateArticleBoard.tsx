8b2507ac051cc2ecbe4bccc2aa2333f7d8b15b96
Gold-gyu <lkg2097@naver.com>
2024-08-12 22:19:34 +0900

    게시글 폼 생성

--- frontend/src/components/CreateArticleBoard/CreateArticleBoard.tsx
+++ frontend/src/components/CreateArticleBoard/CreateArticleBoard.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './CreateArticleBoard.css';
 
 const CreateArticleBoard = () => {
-  return <div>보드 생성 폼</div>;
+  return (
+    <div>
+      <h1>보드 생성 폼</h1>
+      <div className="create-article-board">
+        <div>게시글 셍상폼</div>
+        <div className="create-article-button-box">
+          <button>게시하기</button>
+          <button>취소하기</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateArticleBoard;
