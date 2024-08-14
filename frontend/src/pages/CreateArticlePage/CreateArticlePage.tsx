4d6443696405ddb5e3fafb11f5f3c377356ffd33
Gold-gyu <lkg2097@naver.com>
2024-08-14 13:27:01 +0900

    게시글 작성 페이지 제작

---
+++ frontend/src/pages/CreateArticlePage/CreateArticlePage.tsx
@@ -0,0 +1,12 @@
+import CreateArticleBoard from '@/components/Write/CreateArticleBoard/CreateArticleBoard';
+import React from 'react';
+
+const CreateArticlePage = () => {
+  return (
+    <div>
+      <CreateArticleBoard />
+    </div>
+  );
+};
+
+export default CreateArticlePage;
