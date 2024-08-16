b0f8fc4e3bebb7490f2b19f7a061b098e0f6bc92
Gold-gyu <lkg2097@naver.com>
2024-08-16 21:01:40 +0900

    게시글 작성 api 컨트롤러

---
+++ Backend/src/main/java/com/practice/backend/controller/BlogApiController.java
@@ -0,0 +1,26 @@
+package com.practice.backend.controller;
+
+import com.practice.backend.Article;
+import com.practice.backend.dto.AddArticleRequest;
+import com.practice.backend.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    // 요청 본문 값 매칭
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
