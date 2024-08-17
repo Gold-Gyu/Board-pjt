31b15cffd689aaea825e27ac662f7ed6ba28a537
Gold-gyu <lkg2097@naver.com>
2024-08-17 12:13:11 +0900

    게시글 전체 조회 로직 구현

--- Backend/src/main/java/com/practice/backend/service/BlogService.java
+++ Backend/src/main/java/com/practice/backend/service/BlogService.java
@@ -6,6 +6,8 @@ import com.practice.backend.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // Final이 붙거나 @Notnull이 붙은 필드의 생성자 추가
 @Service // 빈으로 추가
 public class BlogService {
@@ -14,5 +16,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
