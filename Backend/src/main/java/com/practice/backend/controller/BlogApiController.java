21a200cff5b6522bedf9caae29b2923f2eaaaf64
Gold-gyu <lkg2097@naver.com>
2024-08-17 12:13:21 +0900

    게시글 전체 조회 api 구현

--- Backend/src/main/java/com/practice/backend/controller/BlogApiController.java
+++ Backend/src/main/java/com/practice/backend/controller/BlogApiController.java
@@ -1,14 +1,18 @@
 package com.practice.backend.controller;
 
+import com.practice.backend.dto.ArticleFindAllResponse;
 import com.practice.backend.entity.Article;
 import com.practice.backend.dto.AddArticleRequest;
 import com.practice.backend.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +26,15 @@ public class BlogApiController {
         System.out.println(savedArticle.getCategory());
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    // 게시글 조회
+    public ResponseEntity<List<ArticleFindAllResponse>> getAllArticles() {
+        List<ArticleFindAllResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleFindAllResponse::new)
+                .toList();
+        return ResponseEntity.ok().body(articles);
     }
 }
