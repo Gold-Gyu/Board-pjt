9821f1b3983eea95b3d813ad056db89f80859c11
Gold-gyu <lkg2097@naver.com>
2024-08-20 20:10:26 +0900

    backend

--- Backend/src/main/java/com/practice/backend/entity/User.java
+++ Backend/src/main/java/com/practice/backend/entity/User.java
@@ -2,4 +2,8 @@ package com.practice.backend.entity;
 
 public class User {
     private long userId;
+    private String userName;
+    private String password;
+    private String email;
+    private String phone;
 }
