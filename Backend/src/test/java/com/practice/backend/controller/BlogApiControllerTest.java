dd381799d66a850114dcf225a688b2e29f631af0
Gold-gyu <lkg2097@naver.com>
2024-08-24 11:52:07 +0900

    테스트코드 잠시 비활성화

--- Backend/src/test/java/com/practice/backend/controller/BlogApiControllerTest.java
+++ Backend/src/test/java/com/practice/backend/controller/BlogApiControllerTest.java
@@ -1,93 +1,93 @@
-package com.practice.backend.controller;
-
-import com.practice.backend.entity.Article;
-import org.junit.jupiter.api.Assertions;
-import static org.assertj.core.api.Assertions.assertThat; // AssertJ를 사용한 assertThat
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.practice.backend.dto.AddArticleRequest;
-import com.practice.backend.enums.Category;
-import com.practice.backend.repository.BlogRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.util.List;
-
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-
-
-class BlogApiControllerTest {
-
-    @Autowired
-    protected MockMvc mockMvc;
-
-    @Autowired
-    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    BlogApiController blogApiController;
-    @Autowired
-    private BlogRepository blogRepository;
-
-    @BeforeEach // 테스트 전 실행 코드
-    public void mockMvcSetup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-        blogRepository.deleteAll();
-    }
-
-    @DisplayName("addArticle: 블로그 글 추가에 성공한다")
-    @Test
-    public void addArticle() throws Exception {
-        //given
-        final String url = "/api/articles";
-        final String title = "title";
-        final String content = "content";
-        final Category category = Category.valueOf("NOTICE");
-        final String author = "author";
-        final int likeCount = 0;
-        final int commentCount = 0;
-
-        final AddArticleRequest userRequest = new AddArticleRequest(title, content, category, author, likeCount, commentCount);
-
-        // 객체 JSON으로 직렬화
-        final String requestBody = objectMapper.writeValueAsString(userRequest);
-
-        // when
-        ResultActions result = mockMvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(requestBody)
-        );
-
-
-        //then
-
-        result.andExpect(status().isCreated());
-
-        List<Article> articles = blogRepository.findAll();
-
-        assertThat(articles.size());
-        assertThat(articles.get(0).getTitle().equals(title));
-        assertThat(articles.get(0).getContent()).isEqualTo(content);
-
-    }
-}
+//package com.practice.backend.controller;
+//
+//import com.practice.backend.entity.Article;
+//import org.junit.jupiter.api.Assertions;
+//import static org.assertj.core.api.Assertions.assertThat; // AssertJ를 사용한 assertThat
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.practice.backend.dto.AddArticleRequest;
+//import com.practice.backend.enums.Category;
+//import com.practice.backend.repository.BlogRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MockMvcBuilder;
+//import org.springframework.test.web.servlet.ResultActions;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//
+//import java.util.List;
+//
+//import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//
+//
+//class BlogApiControllerTest {
+//
+//    @Autowired
+//    protected MockMvc mockMvc;
+//
+//    @Autowired
+//    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
+//
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
+//
+//    @Autowired
+//    BlogApiController blogApiController;
+//    @Autowired
+//    private BlogRepository blogRepository;
+//
+//    @BeforeEach // 테스트 전 실행 코드
+//    public void mockMvcSetup() {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+//        blogRepository.deleteAll();
+//    }
+//
+//    @DisplayName("addArticle: 블로그 글 추가에 성공한다")
+//    @Test
+//    public void addArticle() throws Exception {
+//        //given
+//        final String url = "/api/articles";
+//        final String title = "title";
+//        final String content = "content";
+//        final Category category = Category.valueOf("NOTICE");
+//        final String author = "author";
+//        final int likeCount = 0;
+//        final int commentCount = 0;
+//
+//        final AddArticleRequest userRequest = new AddArticleRequest(title, content, category, author, likeCount, commentCount);
+//
+//        // 객체 JSON으로 직렬화
+//        final String requestBody = objectMapper.writeValueAsString(userRequest);
+//
+//        // when
+//        ResultActions result = mockMvc.perform(post(url)
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .content(requestBody)
+//        );
+//
+//
+//        //then
+//
+//        result.andExpect(status().isCreated());
+//
+//        List<Article> articles = blogRepository.findAll();
+//
+//        assertThat(articles.size());
+//        assertThat(articles.get(0).getTitle().equals(title));
+//        assertThat(articles.get(0).getContent()).isEqualTo(content);
+//
+//    }
+//}
