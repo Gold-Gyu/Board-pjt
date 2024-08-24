a9fd09f83a2601def880f16f51efc57a14e7ae86
Gold-gyu <lkg2097@naver.com>
2024-08-24 11:55:25 +0900

    게시글 상세 페이지 백엔드 연결 + 조회 예외 처리

--- frontend/src/pages/ArticlePage/ArticlePage.tsx
+++ frontend/src/pages/ArticlePage/ArticlePage.tsx
@@ -1,33 +1,58 @@
 import useMovePage from '@/hooks/useMovePage';
 import '@/styles/Button.css';
 import './ArticlePage.css';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { Article } from '@/types/Article';
+import instance from '@/apis/instance';
 
 const ArticlePage = () => {
-  const location = useLocation();
-  const post = location.state;
+  const params = useParams();
+  const [article, setArticle] = useState<Article | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await instance.get(`/${params.spid}`);
+        setArticle(response.data);
+      } catch {
+        console.log('error');
+      }
+    };
+    if (params.spid) {
+      fetchData();
+    }
+  }, [params.spid]);
+
+  // const location = useLocation();
+  // console.log(location);
+
+  // const post = location.state;
   const { goBack } = useMovePage();
   return (
     <div>
       <div className="table-container">
-        {post && (
+        {article ? (
           <div>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
-            <p>작성자: {post.author}</p>
-            <p>작성일: {post.date}</p>
-            <p>조회수: {post.views}</p>
+            <div>
+              <h1>{article.title}</h1>
+              <div dangerouslySetInnerHTML={{ __html: article.content }} />
+              <p>작성자: {article.author}</p>
+              <p>작성일: {article.publishDate}</p>
+              <p>조회수: {article.views}</p>
+            </div>
+            <div className="article-page">
+              <button className="blue-button" onClick={() => goBack()}>
+                뒤로가기
+              </button>
+            </div>
+            <div className="table-container" style={{ marginTop: '30px' }}>
+              <div>댓글창</div>
+            </div>
           </div>
+        ) : (
+          <div>해당 게시글은 존재하지 않습니다.</div>
         )}
-
-        <div className="article-page">
-          <button className="blue-button" onClick={() => goBack()}>
-            뒤로가기
-          </button>
-        </div>
-      </div>
-      <div className="table-container" style={{ marginTop: '30px' }}>
-        <div>게시글 목록 5개정도 보여주기</div>
       </div>
     </div>
   );
