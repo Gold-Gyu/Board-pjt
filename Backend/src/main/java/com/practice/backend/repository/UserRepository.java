c8d0b4c865bc04f438e038916384eb6c90f25cbc
Gold-gyu <lkg2097@naver.com>
2024-09-04 17:29:19 +0900

    email로 찾는 JPA

---
+++ Backend/src/main/java/com/practice/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.practice.backend.repository;
+
+import com.practice.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); // email로 정보를 가져옴
+}
