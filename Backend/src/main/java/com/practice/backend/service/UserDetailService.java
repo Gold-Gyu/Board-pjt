9417793ca29955e8a86829dac746b6dd3427b191
Gold-gyu <lkg2097@naver.com>
2024-09-04 17:29:43 +0900

    email로 찾아서 있는지 없는지 확인

---
+++ Backend/src/main/java/com/practice/backend/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.practice.backend.service;
+
+import com.practice.backend.entity.User;
+import com.practice.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
