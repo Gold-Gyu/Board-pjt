67859687630f03063f25487a41ce99392d3e1090
Gold-gyu <lkg2097@naver.com>
2024-09-06 21:47:27 +0900

    불필요 어노테이션 제거

--- Backend/src/main/java/com/practice/backend/dto/AddUserRequest.java
+++ Backend/src/main/java/com/practice/backend/dto/AddUserRequest.java
@@ -4,8 +4,7 @@ import lombok.*;
 
 @Getter
 @Setter
-@NoArgsConstructor  // 기본 생성자 추가
-@AllArgsConstructor // 모든 필드를 받는 생성자 추가
+
 public class AddUserRequest {
     private String nickName;
     private String email;
