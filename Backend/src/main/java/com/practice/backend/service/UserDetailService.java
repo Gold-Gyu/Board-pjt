654315b14b047eea8ce5c71c2c48b0129bd2991e
Gold-gyu <lkg2097@naver.com>
2024-09-06 21:46:38 +0900

    에러 타입 로그

--- Backend/src/main/java/com/practice/backend/service/UserDetailService.java
+++ Backend/src/main/java/com/practice/backend/service/UserDetailService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RequiredArgsConstructor
 @Service
@@ -13,8 +15,15 @@ public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public User loadUserByUsername(String email) {
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        System.out.println("Received email: " + email);
+        System.out.println("Request parameters: " + ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getParameterMap());
+        
+        if (email == null || email.isEmpty()) {
+            throw new UsernameNotFoundException("이메일이 비어있습니다.");
+        }
+
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException(email));
+                .orElseThrow(() -> new UsernameNotFoundException(email + " 해당 메일을 찾을 수 없습니다."));
     }
 }
