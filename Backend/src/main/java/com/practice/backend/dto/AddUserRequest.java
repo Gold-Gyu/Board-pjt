c344ded696aa942a0aa97e3181f81e39bdaf0096
Gold-gyu <lkg2097@naver.com>
2024-09-09 22:13:34 +0900

    User DTO 재설정

--- Backend/src/main/java/com/practice/backend/dto/AddUserRequest.java
+++ Backend/src/main/java/com/practice/backend/dto/AddUserRequest.java
@@ -9,4 +9,8 @@ public class AddUserRequest {
     private String nickName;
     private String email;
     private String password;
+    private int visitCount;
+    private int articleCount;
+    private int followerCount;
+    private int followedCount;
 }
