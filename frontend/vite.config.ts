3ea42a4a62c1639ae865ae5613b265038063d2d6
Gold-gyu <lkg2097@naver.com>
2024-09-09 22:11:59 +0900

    cors 프론트엔드 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,13 +10,19 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
   // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8080',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  //   cors: {
+  //     origin: 'http://localhost:5173', // 올바른 Origin 설정
+  //     credentials: true,               // 인증 정보 허용 (쿠키 등)
+  //   }
+  // }
 });
