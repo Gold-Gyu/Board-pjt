bf4649935b9e8818f9f4b3c6ad1108377e2e0400
Gold-gyu <lkg2097@naver.com>
2024-09-09 22:16:43 +0900

    불필요한 println 제거

--- Backend/src/main/java/com/practice/backend/service/UserService.java
+++ Backend/src/main/java/com/practice/backend/service/UserService.java
@@ -13,29 +13,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Transactional
     public Long save(AddUserRequest dto) {
-
         if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
             throw new IllegalArgumentException("Password cannot be null or empty");
         }
-
-        System.out.println("Received password: " + dto.getPassword());
-
 
         User user = User.builder()
                 .email(dto.getEmail())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword()))  // 비밀번호를 암호화
                 .nickName(dto.getNickName())
                 .build();
-        System.out.println(user.getEmail( ));
-        System.out.println(user.getUsername());
-        System.out.println(user.getPassword( ));
-        System.out.println(user.getNickName( ));
-        System.out.println(user.getUserId( ));
 
         return userRepository.save(user).getUserId();
-
-
     }
 }
+
